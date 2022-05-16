30ad0d7db4bdf02f7e8994aaf7b4ab46861f4807
puseyr <pusey.ronald@gmail.com>
2022-05-15 22:07:10 -0400

    Adding files

---
+++ OLD/RemoveEntryHostsCopy (regex).ps1
@@ -0,0 +1,21 @@
+﻿Set-ExecutionPolicy Unrestricted 
+
+  $computers = Get-Content "\\nas\Install\Scripts\Powershell\HostsFileUpdate\computerlist.txt"
+
+  $computers | foreach-object ({
+  If (Test-Connection $_)
+  {
+    invoke-command -ComputerName $_ -ScriptBlock {
+    (Get-Content "$($env:windir)\system32\Drivers\etc\hosts")  -replace '(0.0.0.0|messenger.yahoo.com|0.0.0.0		messenger.yahoo.com |10.208.1.16 autodiscover.glencore-us.com| 		  |10.208.1.16	autodiscover.glencore-us.com|10.208.1.16	autodiscover.glencore-us.com |10.208.1.16	autodiscover.glencore-us.com 
+ )',' ' |
+    Out-File "$($env:windir)\system32\Drivers\etc\hosts" -Force
+    Write-Host "$($env:computername) Hosts file modified"
+  }
+  }
+  else
+  {
+    Write-Host "$_ Hosts file modified"
+  }
+  Out-File "\\usnycsrv2uwfp1\upf\rpusey-sa\WF\RF\Documents\PowerShell building\RemoteEntryHostsCopy"\$computers.txt -Force
+}
+)
---
+++ RemoteHostFileReplace.ps1
@@ -0,0 +1,2 @@
+﻿$computer = Read-Host 'Computer Name?'
+Copy-Item -Path "\\nas\install\Scripts\VPNHostsWorkaround\hosts" -Destination "\\$computer\c$\windows\system32\drivers\etc\" -Force
